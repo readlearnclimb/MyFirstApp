aa04d21e23a5b75fa41a003cad9cfb6af41a86f1
Read Learn Climb <raphaelkang@gmail.com>
2019-01-19 16:23:59 -0800

    my first app

--- app/src/main/java/com/readlearnclimb/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/readlearnclimb/myfirstapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.readlearnclimb.myfirstapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonBottom.setOnClickListener {
+            val secondActivity = Intent(this, SecondActivity::class.java)
+            startActivity(secondActivity)
+        }
+
     }
 }
